2e0ba76f163e32cdd807b218bd3cc53276e0e512
luigifaticoso <faticosoluigi@gmail.com>
2020-11-27 14:08:52 +0100

    added agumentation with comments

--- final.py
+++ final.py
@@ -30,6 +30,8 @@ from mrcnn.visualize import display_instances
 import matplotlib.pyplot as plt
 import keras
 from keras.callbacks import Callback
+import imgaug
+from imgaug import augmenters as iaa
 # Root directory of the project
 ROOT_DIR = os.path.abspath("")
 
@@ -44,6 +46,38 @@ COCO_WEIGHTS_PATH = os.path.join(ROOT_DIR, "mask_rcnn_coco.h5")
 # Directory to save logs and model checkpoints, if not provided
 # through the command line argument --logs
 DEFAULT_LOGS_DIR = os.path.join(ROOT_DIR, "logs")
+
+# Agumentations
+
+augmentation = iaa.Sometimes(.667, iaa.Sequential([
+    iaa.Fliplr(0.5), # horizontal flips
+    iaa.Crop(percent=(0, 0.1)), # random crops
+    # Small gaussian blur with random sigma between 0 and 0.25.
+    # But we only blur about 50% of all images.
+    iaa.Sometimes(0.5,
+        iaa.GaussianBlur(sigma=(0, 0.25))
+    ),
+    # Strengthen or weaken the contrast in each image.
+    iaa.ContrastNormalization((0.75, 1.5)),
+    # Add gaussian noise.
+    # For 50% of all images, we sample the noise once per pixel.
+    # For the other 50% of all images, we sample the noise per pixel AND
+    # channel. This can change the color (not only brightness) of the
+    # pixels.
+    iaa.AdditiveGaussianNoise(loc=0, scale=(0.0, 0.05*255)),
+    # Make some images brighter and some darker.
+    # In 20% of all cases, we sample the multiplier once per channel,
+    # which can end up changing the color of the images.
+    iaa.Multiply((0.8, 1.2)),
+    # Apply affine transformations to each image.
+    # Scale/zoom them, translate/move them, rotate them and shear them.
+    iaa.Affine(
+        scale={"x": (0.8, 1.2), "y": (0.8, 1.2)},
+        #translate_percent={"x": (-0.2, 0.2), "y": (-0.2, 0.2)},
+        rotate=(-180, 180),
+        #shear=(-8, 8)
+    )
+], random_order=True)) # apply augmenters in random order
 
 ############################################################
 #  Configurations
@@ -232,7 +266,8 @@ def train(model):
     print("Training network heads")
     model.train(dataset_train, dataset_val,
                 learning_rate=config.LEARNING_RATE,
-                epochs=300,
+                augmentation=augmentation,
+                epochs=100,
                 custom_callbacks = [tensorboard_callback,mean_average_precision_callback],
                 layers='heads')
 
